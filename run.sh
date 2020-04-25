904e2ccb177ce935b21930834caef55316a419da
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2020-04-25 18:17:33 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-service mysql start 1>/dev/null
-echo "[#] Challenge can be accessed at: http://$(hostname -I)"
-php artisan serve --host=0.0.0.0 --port=80 1>/dev/null
+service mysql start 1>/dev/null && \
+	mysql < sqlCreds && \
+	php artisan migrate && \
+	chmod +x queries.sh && \
+	./queries.sh && \
+	echo "[#] Challenge can be accessed at: http://$(hostname -I)" && \
+	php artisan serve --host=0.0.0.0 --port=80 1>/dev/null
